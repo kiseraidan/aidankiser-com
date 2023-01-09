0b1061d19ea67c0d27cd4f7354f51f84fb670878
Aidan Kiser <kiseraidan@gmail.com>
2023-01-09 16:34:41 +0900

    poop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> stinky eggs.
+          Poop.
         </p>
         <a
           className="App-link"
